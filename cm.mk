061d93720ce3833e0d678edc65797a52d520ace1
Arne Coucheron <arco68@gmail.com>
2014-02-14 02:17:38 +0100

    serranoltexx: Change inherit order
     * Move comon_full_phone.mk to top. Otherwise default notification
       and alarm tone will be completely wrong
    
    Change-Id: Ie75654989ff27b0ea639157fd79c3f5e18355919

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte BUILD_FINGERPRINT="samsung/serranoltexx/serranolte:4.2.2/JDQ39/I9195XXUAMG4:user/release-keys" PRIVATE_BUILD_DESC="serranoltexx-user 4.2.2 JDQ39 I9195XXUAMG4 release-keys"
 
