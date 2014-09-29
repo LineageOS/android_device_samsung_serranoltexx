9a150047e5065a89fd73294f919e35ea45604bf3
Arne Coucheron <arco68@gmail.com>
2014-09-29 04:56:34 +0200

    serranoltexx: Fix product and device name
    Change-Id: I43e09cfd3a2a995322117d700152886466195fd4

--- cm.mk
+++ cm.mk
@@ -6,4 +6,6 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
+
 PRODUCT_NAME := cm_serranoltexx
