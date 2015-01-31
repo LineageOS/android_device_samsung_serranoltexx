ff44df72ab925ab6c6b5904e5cea3e359639fb32
Arne Coucheron <arco68@gmail.com>
2015-01-31 07:39:19 +0100

    serranoltexx: Add custom AAPT config
    Change-Id: Ic5dcc429e8da1b9c0af69134910fb8986833870c

--- cm.mk
+++ cm.mk
@@ -6,6 +6,10 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 $(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
 
 PRODUCT_NAME := cm_serranoltexx
