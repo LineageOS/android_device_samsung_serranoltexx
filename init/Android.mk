b4f3d1ca741317e463d417a512fb0fae72556d8a
Arne Coucheron <arco68@gmail.com>
2015-10-23 00:09:04 +0200

    Initial M bringup
    Change-Id: I659df444a54e38266a12bd273cf0c9bfa02879d5

--- init/Android.mk
+++ init/Android.mk
@@ -5,7 +5,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_C_INCLUDES := system/core/init
 LOCAL_CFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
-LOCAL_SRC_FILES := init_serranolte.c
+LOCAL_SRC_FILES := init_serranolte.cpp
 LOCAL_MODULE := libinit_serranolte
 
 include $(BUILD_STATIC_LIBRARY)
