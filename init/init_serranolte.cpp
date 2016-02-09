2a4bf11d64155f4de853e30ad7d57edcacf976c4
Arne Coucheron <arco68@gmail.com>
2016-02-09 06:39:09 +0100

    Revert "init: Set device serial number via vendor init"
    This reverts commit 995a7ff43c344c06b6f75e814f8df3591feeaa5e.
    
    Change-Id: I15093622aaccfb62a44cc23f20802aeb6f495fbc

--- init/init_serranolte.cpp
+++ init/init_serranolte.cpp
@@ -1,25 +1,33 @@
 /*
- * Copyright 2016, The CyanogenMod Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+   Copyright (c) 2013, The Linux Foundation. All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h>
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
@@ -27,57 +35,6 @@
 #include "util.h"
 
 #include "init_msm.h"
-
-/* Serial number */
-#define SERIAL_FILE "/efs/FactoryApp/serial_no"
-#define SERIAL_PROP "ro.serialno"
-#define SERIAL_OFFSET 0x00
-#define SERIAL_LENGTH 11
-
-static int read_file(const char *fname, char *data, int max_size)
-{
-    int fd, rc;
-
-    if (max_size < 1)
-        return 0;
-
-    fd = open(fname, O_RDONLY);
-    if (fd < 0) {
-        ERROR("failed to open '%s'\n", fname);
-        return 0;
-    }
-
-    rc = read(fd, data, max_size -1);
-    if ((rc > 0) && (rc < max_size ))
-        data[rc] = '\0';
-    else
-        data[0] = '\0';
-    close(fd);
-
-    return 1;
-}
-
-static void get_serial()
-{
-    int ret = 0;
-    char const *path = SERIAL_FILE;
-    char buf[SERIAL_LENGTH + 1];
-    prop_info *pi;
-
-    if(read_file(path, buf, sizeof(buf))) {
-
-        if (strlen(buf) > 0) {
-            pi = (prop_info*) __system_property_find(SERIAL_PROP);
-            if(pi)
-                ret = __system_property_update(pi, buf,
-                        strlen(buf));
-            else
-                ret = __system_property_add(SERIAL_PROP,
-                        strlen(SERIAL_PROP),
-                        buf, strlen(buf));
-        }
-    }
-}
 
 void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *board_type)
 {
@@ -117,7 +74,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.product.device", "serranoltektt");
     }
 
-    get_serial();
     property_get("ro.product.device", device);
     strlcpy(devicename, device, sizeof(devicename));
     INFO("Found bootloader id %s setting build properties for %s device\n", bootloader, devicename);
