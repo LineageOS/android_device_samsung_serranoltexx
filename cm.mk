39f333d6e263c342493152c3ac169ce7a1cc052f
Arne Coucheron <arco68@gmail.com>
2016-10-12 00:44:02 +0200

    Initial N bringup
    Change-Id: If2f88b8b0062e63ddb1f743495125da7682cacb1

--- cm.mk
+++ cm.mk
@@ -1,11 +1,30 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from serranoltexx device
+$(call inherit-product, device/samsung/serranoltexx/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-$(call inherit-product, device/samsung/serranoltexx/full_serranoltexx.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranoltexx TARGET_DEVICE=serranolte
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_NAME := cm_serranoltexx
+PRODUCT_DEVICE := serranoltexx
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := Samsung
