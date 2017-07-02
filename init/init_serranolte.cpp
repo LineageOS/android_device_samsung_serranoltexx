6f46d3540c872764b6f6df05d000390aa76c7fc8
Michael Bestas <mikeioannina@cyanogenmod.org>
2017-07-02 06:29:58 +0200

    serranoltexx: Allow vendor init to override props
    Change-Id: I45ae9d12b62bc7a0258d7386d35c08153ea4a7ae

--- init/init_serranolte.cpp
+++ init/init_serranolte.cpp
@@ -35,6 +35,20 @@
 #include "log.h"
 #include "util.h"
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+void property_override(char const prop[], char const value[])
+{
+    prop_info *pi;
+
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
 void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
