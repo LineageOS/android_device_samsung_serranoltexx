7d6d92e709b3c32f8e937aaeb4a0186bed5e59f0
Arne Coucheron <arco68@gmail.com>
2017-10-09 02:26:42 +0200

    serranoltexx: Inherit from vendor/lineage
    Change-Id: If4a7fa7234e60e0735c537fd4e37e7e1962d21bd

--- lineage.mk
+++ lineage.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/serranoltexx/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_serranoltexx
 PRODUCT_DEVICE := serranoltexx
