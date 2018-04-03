83480e8c0b0c28c668e1d99511bed0d55c1ad154
Arne Coucheron <arco68@gmail.com>
2018-04-03 23:44:23 +0200

    serranoltexx: Unbreak fingerprint after https://review.lineageos.org/#/c/208868/
    Change-Id: Idb818789ad9474636bcc5db798eedd6b9c0065ff

--- init/init_serranolte.cpp
+++ init/init_serranolte.cpp
@@ -48,6 +48,12 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void property_override_dual(char const system_prop[], char const vendor_prop[], char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+}
+
 void vendor_load_properties()
 {
     char platform[PROP_VALUE_MAX];
@@ -64,22 +70,22 @@ void vendor_load_properties()
 
     if (strstr(bootloader, "I257M")) {
         /* serranoltebmc */
-        property_override("ro.build.fingerprint", "samsung/serranoltebmc/serranoltebmc:4.4.2/KOT49H/I257MVLUBNE6:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/serranoltebmc/serranoltebmc:4.4.2/KOT49H/I257MVLUBNE6:user/release-keys");
         property_override("ro.build.description", "serranoltebmc-user 4.4.2 KOT49H I257MVLUBNE6 release-keys");
-        property_override("ro.product.model", "SGH-I257M");
-        property_override("ro.product.device", "serranoltebmc");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SGH-I257M");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "serranoltebmc");
     } else if (strstr(bootloader, "I9195")) {
         /* serranoltexx */
-        property_override("ro.build.fingerprint", "samsung/serranoltexx/serranolte:4.4.2/KOT49H/I9195XXUCNE6:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "samsung/serranoltexx/serranolte:4.4.2/KOT49H/I9195XXUCNE6:user/release-keys");
         property_override("ro.build.description", "serranoltexx-user 4.4.2 KOT49H I9195XXUCNE6 release-keys");
-        property_override("ro.product.model", "GT-I9195");
-        property_override("ro.product.device", "serranoltexx");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "GT-I9195");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "serranoltexx");
     } else if (strstr(bootloader, "E370K")) {
         /* serranoltektt */
-        property_override("ro.build.fingerprint", "samsung/serranoltektt/serranoltektt:4.4.4/KTU84P/E370KKTU2BNK5:user/release-keys");
+        property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint",  "samsung/serranoltektt/serranoltektt:4.4.4/KTU84P/E370KKTU2BNK5:user/release-keys");
         property_override("ro.build.description", "serranoltektt-user 4.4.4 KTU84P E370KKTU2BNK5 release-keys");
-        property_override("ro.product.model", "SHV-E370K");
-        property_override("ro.product.device", "serranoltektt");
+        property_override_dual("ro.product.model", "ro.vendor.product.model", "SHV-E370K");
+        property_override_dual("ro.product.device", "ro.vendor.product.device", "serranoltektt");
     }
 
     property_get("ro.product.device", device, NULL);
